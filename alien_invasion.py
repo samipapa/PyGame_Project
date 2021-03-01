473adf02a4b665ff3833c8456940f648f56caebd
Samantha Papageorge <samantha_papageorge1@baylor.edu>
2021-02-28 23:09:48 -0600

    commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,21 @@ import pygame
 
 # Class to manage game assets and behavior
 class AlienInvasion:
-    def__init__(self):
-    pygame.init()
-    self.screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
