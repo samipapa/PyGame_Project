60937cd3d33ce6c62bcc9d60f5eade11c6db2d2f
Samantha Papageorge <samantha_papageorge1@baylor.edu>
2021-02-28 22:53:52 -0600

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,10 @@
+# Imports
+import sys
+import pygame
+
+# Class to manage game assets and behavior
+class AlienInvasion:
+    def__init__(self):
+    pygame.init()
+    self.screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
