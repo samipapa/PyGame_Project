a9f729d352b7c11e5aa7c3939f895eafda64260b
Samantha Papageorge <samantha_papageorge1@baylor.edu>
2021-03-10 13:43:03 -0600

    final edits

--- game_stats.py
+++ game_stats.py
@@ -4,5 +4,11 @@ class GameStats:
         self.reset_stats()
         self.game_active = True
 
+        self.game_active = False
+
+        self.high_score = 0
+
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
